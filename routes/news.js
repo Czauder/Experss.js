a9b8df1b92a4ac6b7da358f516fc500f9bd5d0e3
pczauderma <pczauderna@prescientco.com>
2019-10-05 12:25:16 +0200

    news filter front

--- routes/news.js
+++ routes/news.js
@@ -4,14 +4,23 @@ const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res) => {
+  const search = req.query.search || '';
+
   const findNews = News
-  .find()
-  .sort({created: -1 });
+    .find({ title: new RegExp(search.trim(), 'i')
+    })
+    .sort({
+      created: -1
+    });
 
   findNews.exec((err, data) => {
     console.log(data);
 
-    res.render('news', { title: 'News', data });
+    res.render('news', {
+      title: 'News',
+      data,
+      search
+    });
   });
 });
 
