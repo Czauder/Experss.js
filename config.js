27da34de65d5e65ef31cb98a50f250da78ac8e48
pczauderma <pczauderna@prescientco.com>
2019-10-06 11:10:17 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:eV07EunczFGHqbmX@cluster0-czbhp.mongodb.net/admin?retryWrites=true&w=majority',
     keySession: ['YOURKEY'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
